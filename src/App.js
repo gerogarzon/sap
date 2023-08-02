6da2bb988108f97c4b35e0de2da455cf34efac73
gerogarzon <gerogarzon@gmail.com>
2023-08-02 20:07:48 -0300

    listo exponential inception

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import sap from "./assets/sap.jpg"
-import './App.css';
+import sap from "./assets/descarga2.png";
+import "./App.css";
 
 function App() {
   return (
-    <>
-    <nav className="navbar navbar-dark bg-dark">
-      <div className="container-fluid">
-        <img className="navbar-brand mb-0 h1 imagen_sap" src={sap} alt="sap logo"></img>
-        <h5 className="nav_titulo">Academy for Customer Success – S-CSP</h5>
-        <h5 className="nav_titulo">Program 2023</h5>
+    <div>
+      <nav className="navbar navbar-dark ">
+        <div className="container-fluid">
+          <img
+            className="navbar-brand mb-0 h1 imagen_sap"
+            src={sap}
+            alt="sap logo"
+          ></img>
+          <h5 className="nav_titulo"> Exponential Inception 🚀 </h5>
+          <h5 className="nav_titulo">2023</h5>
+        </div>
+      </nav>
+      <h4 className="titulo_principal ">Applicant: Geronimo, Garzon</h4>
+      <div className="container">
+        {/* <div className="body"></div> */}
+        <iframe
+          width="853"
+          height="450"
+          src="https://www.youtube.com/embed/LQWAZCa5qyg "
+          title="13 de octubre de 2022"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+          // className="video"
+        ></iframe>
       </div>
-    </nav>
-    <h3 className="titulo_principal">Job Interview</h3>
-    <h4 className="titulo_principal">Applicant: Geronimo, Garzon </h4>
-    <div className="container">
-    <iframe width="853" height="450" src="https://www.youtube.com/embed/ST6KbIoNMQU" title="13 de octubre de 2022" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
-</>
   );
 }
 
